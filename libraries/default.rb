3ef17fa67f81aa1eda5ab1cebdcaa680588dcabc
Baptiste Courtois <b.courtois@criteo.com>
2017-10-16 09:07:47 +0200

    Exposes PCI devices info for Windows nodes
    Use the PCI enumeration from Windows Registry to list all PCI devices.

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,6 +1,13 @@
 # Provides methods to fetch PCI information
 module PCI
-  def self.devices(_node)
-    ::Mash.new
+  def self.devices(node)
+    case node['os']
+    when 'windows'
+      require_relative 'windows.rb'
+      Windows.pci_devices
+    else
+      ::Chef::Log.warn "[PCI] #{node['os']} is not a supported Operating System."
+      nil
+    end
   end
 end
