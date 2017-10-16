28b43fcbc02229de44af0df40cba25bf3fb67ad5
Baptiste Courtois <b.courtois@criteo.com>
2017-10-16 09:30:54 +0200

    Release 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,9 @@
 
 This file is used to list changes made in each version of the pci cookbook.
 
+## Version 0.1.0
+- Annih - Expose PCI attributes on Linux
+- Annih - Expose PCI attributes on Windows
+
 ## Version 0.0.0
 - Annih - Initial commit
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'b.courtois@criteo.com'
 license          'Apache-2.0'
 description      'Expose PCI information as automatic attributes'
 long_description ::IO.read(::File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.0'
+version          '0.1.0'
 
 supports         'centos', '>= 6.0'
 supports         'redhat', '>= 6.0'
